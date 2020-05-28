0add78ec39dd2f76803eae4b2150df0f4f718f77
gimun2 <wjdrlans4000@naver.com>
2020-05-28 17:34:45 +0900

    initial commit

---
+++ OpenLayersWebViewExpo/src/App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React,{useEffect, useRef } from 'react';
+import MyMap from "./Screen/map";
+
+
+
+
+interface Props {}
+
+const App = ({  }: Props) => {
+  return (
+  <>
+    <MyMap />
+  </>
+  );
+};
+
+export default App;

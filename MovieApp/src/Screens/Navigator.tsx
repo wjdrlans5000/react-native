29e1aaec142e87ae6522bc4f38dc38796d598061
gimun2 <wjdrlans4000@naver.com>
2020-05-28 16:24:15 +0900

    f

--- MovieApp/src/Screens/Navigator.tsx
+++ MovieApp/src/Screens/Navigator.tsx
@@ -1,20 +1,13 @@
 import React,{useRef } from 'react';
-import AsyncStorage from '@react-native-community/async-storage';
 import {NavigationContainer} from '@react-navigation/native';;
 import {createStackNavigator} from '@react-navigation/stack';
-import Styled from 'styled-components/native';
 //createStackNavigator : 영화 리스트 화면에서 영화를 선택하면 영화의 상세 페이지를 보여주기 위해 사용할 예정, 화면을 쌓아서(stack) 표시
 //NavigationContainer : 내비게이션을 다루기 위한 State, 링크 등을 관리한다. 마지막에 NavigationContainer로 내비게이션을 감싸서 제공
 import CheckLogin from '~/Screens/CheckLogin';
 import Login from '~/Screens/Login';
 import MovieHome from '~/Screens/MovieHome';
-import MovieDetail from '~/Screens/MovieDetail';
+import MovieDetail from '~/Screens/MovieDetail';  
 
-const StyleButton = Styled.TouchableOpacity`
-  padding: 8px;
-`;
-const Icon = Styled.Image`
-`;
 
 //stack 내비게이터 생성
 const Stack = createStackNavigator();
